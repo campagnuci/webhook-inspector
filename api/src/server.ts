da0c4273f468b82d707805b25f098f46fa3cddb5
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2025-10-27 20:54:32 -0300

    feat: initial commit

---
+++ api/src/server.ts
@@ -0,0 +1,41 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform, type ZodTypeProvider } from 'fastify-type-provider-zod'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+
+import { listWebhooks } from './routes/list-webhooks'
+import { env } from './config/env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0',
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs'
+})
+
+app.register(listWebhooks)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log(`HTTP server running on http://localhost:${env.PORT}!`)
+  console.log('Docs available at http://localhost:3333/docs')
+})
